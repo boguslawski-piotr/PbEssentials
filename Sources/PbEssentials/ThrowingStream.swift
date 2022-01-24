bf67bbd5f1883853b974f266508748eb90af2de8
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-01-24 14:42:57 +0100

    Documentation.

--- Sources/PbEssentials/ThrowingStream.swift
+++ Sources/PbEssentials/ThrowingStream.swift
@@ -2,34 +2,34 @@
 /// Copyright (c) Piotr Boguslawski
 /// MIT license, see License.md file for details.
 
-/// A synchronous sequence generated from an error-throwing closure
-/// partialy compatible with AsyncThrowingStream but for synchronous
-/// environment.
-///
-/// Example:
-///
-///     var sequence = ThrowingStream<T> {
-///         /* code producing data (elements of type T) or
-///            nil when sequence should end */
-///     }
-///
-///     // use thrownig mode
-///     while let element = try sequence.nextThrows() {
-///         /* code consuming data */
-///     }
-///
-///     // use normal mode (standard library does not
-///     // support try/catch in sequences :()
-///     for element in sequence {
-///         /* code consuming data */
-///     }
-///     if let error = sequence.lastError {
-///         throw error
-///     }
-///
+/**
+ A synchronous sequence generated from an error-throwing closure partialy
+ compatible with AsyncThrowingStream but for synchronous environment.
+ 
+ Example:
+ 
+     var sequence = ThrowingStream<T> {
+         /* code producing data (elements of type T) or
+            nil when sequence should end */
+     }
+ 
+     // use thrownig mode
+     while let element = try sequence.nextThrows() {
+         /* code consuming data */
+     }
+ 
+     // use normal mode (standard library does not
+     // support try/catch in sequences :()
+     for element in sequence {
+         /* code consuming data */
+     }
+     if let error = sequence.lastError {
+         throw error
+     }
+ */
 public struct ThrowingStream<Element, Failure> : ErrorReportingSequence, ErrorReportingIteratorProtocol, ThrowingIteratorProtocol where Failure : Error
 {
-    private var produce : () throws -> Element?
+    private let produce : () throws -> Element?
     public private(set) var lastError : Error?
     
     /// Constructs a synchronous throwing stream from a given element-producing closure `produce`.
