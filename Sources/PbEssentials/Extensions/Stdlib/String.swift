e25afb4c9b012a2567186833af410acac484a1ff
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2022-02-11 16:40:55 +0100

    String.satisfy(regexPattern, ...) function.

--- Sources/PbEssentials/Extensions/Stdlib/String.swift
+++ Sources/PbEssentials/Extensions/Stdlib/String.swift
@@ -1,7 +1,17 @@
+import Foundation
 
 public extension String
 {
-    var asPathComponent : String {
+    func asPathComponent() -> String {
         return self.replacingOccurrences(of: "/", with: "-").replacingOccurrences(of: ":", with: "-")
+    }
+}
+
+public extension String
+{
+    func satisfy(regexPattern: String, options: NSRegularExpression.Options = [], matchingOptions: NSRegularExpression.MatchingOptions = []) throws -> Bool {
+        let regex = try NSRegularExpression(pattern: regexPattern, options: options)
+        let nom = regex.numberOfMatches(in: self, options: matchingOptions, range: .init(location: 0, length: self.count))
+        return nom > 0
     }
 }
